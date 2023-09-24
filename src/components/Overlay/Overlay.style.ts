6206f2bd77f9ee582b85afd92ecd5bd2667cd09a
Casana E. Bey <info@expresslayouts.com>
2023-09-24 12:30:59 -0400

    Move side bar to 0

--- src/components/Overlay/Overlay.style.ts
+++ src/components/Overlay/Overlay.style.ts
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const SidebarWrapper = styled.div`
   box-sizing: border-box;
   position: absolute;
-  top: 10px;
-  left: 10px;
+  top: 0px;
+  left: 0px;
   z-index: 1000;
   width: 290px;
-  height: 1060px;
+  height: 1080px;
   background-color: rgba(0, 0, 0, 0.99);
   padding: 10px;
 `;
